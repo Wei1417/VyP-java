ffbc0d6856e559f515c0e0a7b18826ddc50bca45
Wei1417 <pham19002@gmail.com>
2022-09-07 23:17:47 +0700

    Init

--- src/lesson_1/helloword.java
+++ src/lesson_1/helloword.java
@@ -3,5 +3,7 @@ package lesson_1;
 public class helloword {
     public static void main(String[] args){
         System.out.println("Hello");
+        System.out.println("Teo");
+
     }
 }
---
+++ src/lesson_1/variable.java
@@ -0,0 +1,13 @@
+package lesson_1;
+
+public class variable {
+    public static void main(String[] args ) {
+        int myNum = 18;
+        int my_num = 18;
+        int MyNum = 18;
+
+        System.out.println(Byte.MAX_VALUE);
+
+        short myByteNum = 128;
+    }
+}

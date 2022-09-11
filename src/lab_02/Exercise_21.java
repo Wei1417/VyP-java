6a6d9195273f052d5a5945962f278ea749dbba36
Wei1417 <pham19002@gmail.com>
2022-09-11 20:36:29 +0700

    Init

---
+++ src/lab_02/Exercise_21.java
@@ -0,0 +1,30 @@
+package lab_02;
+
+import java.util.Scanner;
+
+public class Exercise_21 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your weight: ");
+        float weight = scanner.nextFloat();
+        System.out.println("Please input your height");
+        float height = scanner.nextFloat();
+        float BMI = (float) weight/(height*2);
+        if (BMI < 18.5) {
+            System.out.println("Underweight");
+        }
+        else
+            if (BMI <= 24.9) {
+                System.out.println("Normal weight");
+            }
+            else
+                if (BMI <= 29.9) {
+                    System.out.println("Overweight");
+                }
+                else
+                    if (BMI >=30) {
+                        System.out.println("Obesity");
+                    }
+    }
+}

ac86933767d6d68f0caa898cf1983568983a2543
Wei1417 <pham19002@gmail.com>
2022-09-19 20:51:46 +0700

    Excersise_33

---
+++ Excercise/lab_03/Excersise_33.java
@@ -0,0 +1,33 @@
+package lab_03;
+
+import java.util.Scanner;
+
+public class Excersise_33 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap so luong phan tu");
+        int n = scanner.nextInt();
+        int [] myIntArray = new int[n];
+        for (int index = 0; index < n; index++) {
+             myIntArray[index] = scanner.nextInt();
+        }
+        for (int i = 0; i < n; i++){
+            int min = Integer.MAX_VALUE;
+            int a = 0;
+            for (int j = i; j < n; j ++){
+                if (myIntArray[j] < min) {
+                    min = myIntArray[j];
+                    a = j;
+                }
+            }
+            int b = myIntArray[i];
+            myIntArray[i] = myIntArray[a];
+            myIntArray[a] = b;
+        }
+        System.out.println("KQ ");
+        for (int index = 0; index < n; index++) {
+            System.out.println(myIntArray[index]);
+
+        }
+    }
+}
